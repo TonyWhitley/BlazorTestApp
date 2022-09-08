946611e2b42c8c538e61570d0158ec159acdbff3
TonyWhitley <Tony_Whitley@hotmail.com>
2022-09-08 20:58:37 +0100

    Enter dice throw

--- BlazorTestApp/BehindTheWheel/GameMechanics/Move.cs
+++ BlazorTestApp/BehindTheWheel/GameMechanics/Move.cs
@@ -20,33 +20,33 @@ namespace BehindTheWheel.GameMechanics
 
         internal void throwDice()
         {
-            if (BlazorIndexPage.Password == "TVR. Blackpool's finest")
-            {
-                fixedThrow(Dice.GetThrow());
-            }
+            fixedThrow(Dice.GetThrow());
         }
         internal void fixedThrow(int dice)
         {
-            var currentSquare = Squares.NextSquare(Player.players[player].CurrentSquare, dice);
-            PlayerModel.playerModel.DisplayThrow(player, dice, currentSquare);
-            // Display the new square
-            PlayerModel.playerModel.Update(player,
-                Player.players[player].Points,
-                Player.players[player].Cash,
-                Squares.board[currentSquare]);
+            if (BlazorIndexPage.Password == "TVR. Blackpool's finest")
+            {
+                var currentSquare = Squares.NextSquare(Player.players[player].CurrentSquare, dice);
+                PlayerModel.playerModel.DisplayThrow(player, dice, currentSquare);
+                // Display the new square
+                PlayerModel.playerModel.Update(player,
+                    Player.players[player].Points,
+                    Player.players[player].Cash,
+                    Squares.board[currentSquare]);
 
-            Tuple<int,int> tuple = Squares.board[currentSquare].OnLandingAction(currentSquare);
-            
-            // Display status after any actions
-            Player.players[player].CurrentSquare = tuple.Item1;
-            Player.players[player].Points += tuple.Item2;
-            Player.players[player].Cash += Squares.board[currentSquare].Cash;
-            Player.players[player].Points += Squares.board[currentSquare].Points;
-            PlayerModel.playerModel.Update(player,
-                Player.players[player].Points,
-                Player.players[player].Cash,
-                Squares.board[Player.players[player].CurrentSquare]);
-            player = (player + 1) % 2;
+                Tuple<int, int> tuple = Squares.board[currentSquare].OnLandingAction(currentSquare);
+
+                // Display status after any actions
+                Player.players[player].CurrentSquare = tuple.Item1;
+                Player.players[player].Points += tuple.Item2;
+                Player.players[player].Cash += Squares.board[currentSquare].Cash;
+                Player.players[player].Points += Squares.board[currentSquare].Points;
+                PlayerModel.playerModel.Update(player,
+                    Player.players[player].Points,
+                    Player.players[player].Cash,
+                    Squares.board[Player.players[player].CurrentSquare]);
+                player = (player + 1) % 2;
+            }
         }
     }
 }
