2191c210a81c148807bde0806dbba8ca44333050
TonyWhitley <Tony_Whitley@hotmail.com>
2022-09-13 17:17:05 +0100

    Stet SyncFusion removal

--- BlazorTestApp/Program.cs
+++ BlazorTestApp/Program.cs
@@ -3,13 +3,13 @@ using BlazorTestApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-//using Syncfusion.Blazor;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-//builder.Services.AddSyncfusionBlazor();
-//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzE0Mjc5QDMyMzAyZTMyMmUzMEswcmRHOHhvelNMVTMxb0IxQmNJb2Q0eHJrNFNLUzZNTnB6bUYrWUxSQVE9");
+builder.Services.AddSyncfusionBlazor();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzE0Mjc5QDMyMzAyZTMyMmUzMEswcmRHOHhvelNMVTMxb0IxQmNJb2Q0eHJrNFNLUzZNTnB6bUYrWUxSQVE9");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
